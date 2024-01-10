29d09c0ba7c1e1536db30f3b7b117cc54d412cfc
Martin Larios <mrlarios_1994@hotmail.es>
2024-01-09 20:23:16 -0600

    adding personal information

---
+++ src/utils/experience.ts
@@ -0,0 +1,87 @@
+export const experience_EN = [
+    {
+        logo: "/projects/gap.jpeg",
+        date: "Jul 2022 - Currently (USA 🇺🇸)",
+        title:
+          "Growth Acceleration Partners - Engineering Team Lead (Senior Software Engineer)",
+        description:
+          "At Growth Acceleration Partners, I currently work as a Senior Software Engineer, using ReactJs+Typescript+NextJs, React Native CLI+Typescript, GraphQL, ApolloClient, Docker, AWS to create and build new websites and mobile applications in addition to this, as Engineering Team Lead, my role is to provide guidance, direction and support to a team of talented engineers. I am responsible for overseeing day-to-day operations, ensuring projects are completed efficiently and on time.",
+        link: "https://www.growthaccelerationpartners.com/",
+      },
+      {
+        logo: "/projects/paktolus.svg",
+        date: "Mar 2022 - Oct 2023 (USA 🇺🇸)",
+        title:
+          "Paktolus Solutions LLC - Senior Full Stack Engineer",
+        description:
+          "At Paktolus Solutions LLC I worked as a Software Engineer building websites and mobile applications using ReactJs+Typescript, React Native + Expo, Docker, AWS, NestJs+Typescript, Strapi Headless CMS. In addition to this, I was working as a Data Engineer which my role was to automate reports using tools such as Looker Studio and BigQuery, in turn, I was using tools to streamline the support process such as Jira, Confluence, Zendesk.",
+        link: "https://www.growthaccelerationpartners.com/",
+      },
+      {
+        logo: "/projects/ns.jpeg",
+        date: "Aug 2021 - Jul 2022 (Nicaragua 🇳🇮)",
+        title: "Nicasource S.A - Software Engineer",
+        description:
+          "At Nicasource S.A I worked as a Software Engineer building websites and mobile apps using ReactJs+Typescript, React Native + Expo, Docker, AWS, NestJs+Typescript, Strapi Headless CMS.",
+        link: "https://www.paktolus.com/",
+      },
+      {
+        logo: "/projects/ld.png",
+        date: "Oct 2019 - August 2021 (Costa Rica 🇨🇷)",
+        title: "Logical Data - Application Software Developer",
+        description:
+          "At Logical Data I worked as an Application Software Developer at Logical Data, built websites and integrated SAP B1 using ReactJs, ASP.net Core 3.1, SAP SDK, Microsoft SQL Server and Azure.",
+        link: "https://ld.co.cr/",
+      },
+      {
+        logo: "/projects/briefcase.webp",
+        date: "Jul 2018 - Oct 2019 (Nicaragua 🇳🇮)",
+        title: "MSCorp - Software Developer",
+        description:
+          "At MSCorp I worked as a software developer creating an ERP using ASP.net Framework, DevExpress and Microsoft SQL Server.",
+      },
+]
+
+export const experience_ES = [
+    {
+        logo: "/projects/gap.jpeg",
+        date: "Julio 2022 - Actualmente (USA 🇺🇸)",
+        title:
+          "Growth Acceleration Partners - Engineering Team Lead (Senior Software Engineer)",
+        description:
+          "En Growth Acceleration Partners, actualmente trabajo como Ingeniero de Software Senior, utilizando ReactJs+Typescript+NextJs, React Native CLI+Typescript, GraphQL, ApolloClient, Docker, AWS para crear y construir nuevos sitios web y aplicaciones móviles además de esto, como Líder del Equipo de Ingeniería, mi papel es proporcionar orientación, dirección y apoyo a un equipo de ingenieros talentosos. Soy responsable de supervisar las operaciones diarias, asegurando que los proyectos se completen de manera eficiente y a tiempo.",
+        link: "https://www.growthaccelerationpartners.com/",
+      },
+      {
+        logo: "/projects/paktolus.svg",
+        date: "Marzo 2022 - Octubre 2023 (USA 🇺🇸)",
+        title:
+          "Paktolus Solutions LLC - Senior Full Stack Engineer",
+        description:
+          "En Paktolus Solutions LLC trabajé como Ingeniero de Software construyendo sitios web y aplicaciones móviles usando ReactJs+Typescript, React Native + Expo, Docker, AWS, NestJs+Typescript, Strapi Headless CMS. Además de esto, estuve trabajando como Ingeniero de Datos el cual mi función era automatizar reportes utilizando herramientas como Looker Studio y BigQuery, a su vez, estuve utilizando herramientas para agilizar el proceso de soporte tales como Jira, Confluence, Zendesk.",
+        link: "https://www.growthaccelerationpartners.com/",
+      },
+      {
+        logo: "/projects/ns.jpeg",
+        date: "Agosto 2021 - Julio 2022 (Nicaragua 🇳🇮)",
+        title: "Nicasource S.A - Software Engineer",
+        description:
+          "Trabajé como Ingeniero de Software construyendo sitios web y aplicaciones móviles usando ReactJs+Typescript, React Native + Expo, Docker, AWS, NestJs+Typescript, Strapi Headless CMS.",
+        link: "https://www.paktolus.com/",
+      },
+      {
+        logo: "/projects/ld.png",
+        date: "Octubre 2019 - Agosto 2021 (Costa Rica 🇨🇷)",
+        title: "Logical Data - Application Software Developer",
+        description:
+          "En Logical Data trabajé como desarrollador de software de aplicaciones en Logical Data, construí sitios web e integré SAP B1 utilizando ReactJs, ASP.net Core 3.1, SAP SDK, Microsoft SQL Server y Azure.",
+        link: "https://ld.co.cr/",
+      },
+      {
+        logo: "/projects/briefcase.webp",
+        date: "Julio 2018 - Octubre 2019 (Nicaragua 🇳🇮)",
+        title: "MSCorp - Software Developer",
+        description:
+          "En MSCorp trabajé como desarrollador de software creando un ERP utilizando ASP.net Framework, DevExpress y Microsoft SQL Server.",
+      },
+]
